24528e52454363e28d3a65144e82ea28399e15c9
zavon <zavon@zavon.org>
2018-05-31 16:20:14 +0000

    Initial commit

---
+++ app/src/main/java/org/zavon/sfst/MainActivity.java
@@ -0,0 +1,56 @@
+package org.zavon.sfst;
+
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentTransaction;
+import android.support.v7.app.AppCompatActivity;
+import android.view.MenuItem;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    private TextView mTextMessage;
+
+    private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
+            = new BottomNavigationView.OnNavigationItemSelectedListener() {
+
+        @Override
+        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+            Fragment selectedFragment = null;
+            switch (item.getItemId()) {
+                case R.id.navigation_hgn:
+                    selectedFragment = HGN.newInstance();
+                    break;
+                case R.id.navigation_wat:
+                    selectedFragment = WAT.newInstance();
+                    break;
+                case R.id.navigation_ols:
+                    selectedFragment = OLS.newInstance();
+                    break;
+                case R.id.navigation_report:
+                    selectedFragment = ReportFragment.newInstance();
+                    break;
+            }
+            FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+            transaction.replace(R.id.frame_layout, selectedFragment);
+            transaction.commit();
+            return false;
+        }
+    };
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
+        navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+
+        FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+        transaction.replace(R.id.frame_layout, HGN.newInstance());
+        transaction.commit();
+    }
+
+}
---
+++ app/src/main/java/org/zavon/sfst/ReportFragment.java
@@ -0,0 +1,29 @@
+package org.zavon.sfst;
+
+import android.content.Context;
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+public class ReportFragment extends Fragment {
+    public static ReportFragment newInstance() {
+        ReportFragment fragment = new ReportFragment();
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_report, container, false);
+    }
+
+}
